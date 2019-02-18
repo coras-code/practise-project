bf56065da9b9a3306538fdc1160fe8679d686c09
Novoda <cora@novoda.com>
2019-02-18 17:44:27 +0000

    Started on datasource

--- practise-project/Recipes/MyIngredientsTableViewController.swift
+++ practise-project/Recipes/MyIngredientsTableViewController.swift
@@ -11,5 +11,27 @@ import UIKit
 class MyIngredientsTableViewController: UITableViewController {
     
     // MARK :- Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientItem", for: indexPath)
+
+        return cell
+        
+    }
+//
+
+//    // MARK:- Table View Data Source
+//    override func tableView(_ tableView: UITableView,
+//                            numberOfRowsInSection section: Int) -> Int {
+//        return 5 }
+//    override func tableView(_ tableView: UITableView,
+//                            cellForRowAt indexPath: IndexPath) ->
+//        UITableViewCell {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientItem", for: indexPath)
+//                return cell }
+//
 }
+
