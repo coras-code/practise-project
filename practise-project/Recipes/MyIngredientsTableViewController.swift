f52e14571dc515c4c17fe4e208d4c5619dcf9575
Novoda <cora@novoda.com>
2019-02-18 10:35:10 +0000

    Added a ingredients table view controller swift files and to the storyboard (connected them) added a prototype cell

---
+++ practise-project/Recipes/MyIngredientsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  practise-project
+//
+//  Created by Novoda on 15/02/2019.
+//  Copyright © 2019 cjm. All rights reserved.
+//
+
+import UIKit
+
+class MyIngredientsTableViewController: UITableViewController {
+    
+    // MARK :- Table View Data Source
+    
+}
