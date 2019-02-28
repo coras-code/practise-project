a51fbb9adb1afff6768fc4cb88cc2521dd7e63f5
Novoda <cora@novoda.com>
2019-02-28 18:09:03 +0000

    Add Data Models (Ingredient and Recipe) with properties

---
+++ practise-project/LeftoverSearch/Ingredient.swift
@@ -0,0 +1,13 @@
+//
+//  Leftover.swift
+//  practise-project
+//
+//  Created by Novoda on 28/02/2019.
+//  Copyright © 2019 cjm. All rights reserved.
+//
+
+import Foundation
+
+struct Ingredient {
+    let name: String
+}
